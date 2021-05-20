d2f1a3218983d810f14447444e8ce1b5c482debe
Tina <me@tina.moe>
2021-05-20 23:52:04 +0200

    Update scripts to always use tools from PATH

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 python -m venv .venv
+./.venv/bin/pip install -U pip
 ./.venv/bin/pip install -r requirements.txt
 ./.venv/bin/pip install uvicorn[standard]
 ./.venv/bin/uvicorn app.main:app --reload --port ${1:-8000}
