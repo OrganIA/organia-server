3a5632d4a1d08f328d7a0b0e7ac9567d31ebcf88
nicolasyapobi <nicolas.yapobi@joltee.fr>
2021-09-07 20:33:34 +0200

    Modify router get hospital

--- app/api/hospitals.py
+++ app/api/hospitals.py
@@ -13,17 +13,14 @@ router = APIRouter(prefix='/hospitals')
 
 @router.get('/')
 async def get_hospitals(name: Optional[str] = None, city: Optional[str] = None):
-    if name == None and city == None:
-        return db.session.query(Hospital).all()
-    filters = db.session.query(Hospital).filter(
+    return db.session.query(Hospital).filter(
         Hospital.name.ilike(f'%{name}%')
         | Hospital.city.ilike(f'%{city}%')
-    ).all() or NotFoundError.r('Hospital not found.')
-    return filters
+    ).all() or db.session.query(Hospital).all() or NotFoundError.r('Hospital not found.')
 
 @router.get('/{hospital_id}')
 async def get_hospital(hospital_id: int):
-    return db.session.query(Hospital).filter_by(id=hospital_id).all() or NotFoundError.r()
+    return db.session.get(Hospital).filter_by(id=hospital_id).first() or NotFoundError.r()
 
 @router.post('/', status_code=201)
 async def create_hospital(hospital: HospitalSchema):
