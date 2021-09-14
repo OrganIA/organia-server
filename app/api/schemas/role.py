865a44a56f075dec232a989c42c730c32772cc9c
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-09-14 16:00:34 +0200

    Changed schemas to one to create a role and one to update a role

--- app/api/schemas/role.py
+++ app/api/schemas/role.py
@@ -17,7 +17,15 @@ class RoleSchema(db.TimedMixin.Schema):
         orm_mode = True
 
 
-class RoleUpdateSchema(RoleSchema):
+class RoleCreateSchema(db.Schema):
+    name: str
+    can_manage_users: bool
+    can_manage_persons: bool
+    can_manage_roles: bool
+    can_manage_hospitals: bool
+    can_invite: bool
+
+class RoleUpdateSchema(db.Schema):
     name: Optional[str]
     can_manage_users: Optional[bool]
     can_manage_persons: Optional[bool]
