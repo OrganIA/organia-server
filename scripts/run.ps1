7830bace914748d62b69b6a5f371afd0be9b9667
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-09-28 09:12:37 +0200

    add autoreload on windows

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 pip install alembic uvicorn[standard]
 
 alembic upgrade head
-uvicorn app.main:app $reload_opts --host 0.0.0.0 --port 8000
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
