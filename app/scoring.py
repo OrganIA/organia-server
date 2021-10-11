190f61f12ca1edca0eefa2eada844feb66cf21be
nicolasyapobi <nicolas.yapobi@joltee.fr>
2021-10-11 04:05:34 +0200

    Update the scoring

--- app/scoring.py
+++ app/scoring.py
@@ -1,13 +1,3 @@
-def heart_scoring(age, rhesus, blood_type_receveir, blood_type_donor):
-    
-    blood_type = compatibility(blood_type_donor)
-    if rhesus == '-':
-        rhesus = -1
-    else:
-        rhesus = 1
-    score = 100 * (rhesus + blood_type + age) / 3
-    return score
-
 def compatibility(blood_type_donor):
     if blood_type_donor == "A":
         blood_type = 1
@@ -18,3 +8,23 @@ def compatibility(blood_type_donor):
     else:
         blood_type = 4
     return blood_type
+
+def organs_priority(organs):
+    if organs == HEART:
+        organs_score = 1
+    elif organs == KIDNEYS:
+        organs_score = 2
+    else:
+        organs_score = 3
+    return organs_score
+
+def heart_scoring(age, rhesus, organs, blood_type_donor):
+    blood_type = compatibility(blood_type_donor)
+    organs_score = organs_priority(organs)
+    if rhesus == '-':
+        rhesus = -1
+    else:
+        rhesus = 1
+        
+    score = 100 * (rhesus + age + (organs_score +  blood_type)) / 3
+    return score
