d559edbda89d74b8ab06258791809f163e64ad0f
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-19 13:23:02 +0200

    Add error handling

--- app/api/messages.py
+++ app/api/messages.py
@@ -28,14 +28,20 @@ async def get_chat_by_id(chat_id: int, logged_user=logged_user):
 @router.get('/messages/{chat_id}', response_model=List[MessageSchema])
 async def get_messages_of_chat(chat_id: int, logged_user=logged_user):
     chat = db.session.get(Chat, chat_id)
-    if (logged_user.id != chat.user_a_id and logged_user.id != chat.user_a_id):
-        raise Unauthorized("You do not have access to this chat.")
-    return db.session.query(Message).filter_by(chat_id=chat_id).all()
+    if not chat:
+        raise NotFoundError("No chat found with this id.")
+    return chat
 
 
 @router.post('/', status_code=201, response_model=ChatSchema)
 async def create_chat(data: ChatCreateSchema, logged_user=logged_user):
     chat = Chat.from_data(data)
+    if not chat:
+        raise InvalidRequest()
+    elif logged_user.id != chat.user_a_id and logged_user.id != chat.user_b_id:
+        raise InvalidRequest(msg="Cannot create a chat for other users.")
+    elif db.session.query(Chat).filter_by(user_a_id=chat.user_a_id, user_b_id=chat.user_b_id).first():
+        raise InvalidRequest(msg="A chat between these users alredy exists.")
     db.session.add(chat)
     db.session.commit()
     return chat
