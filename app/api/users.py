b9afa2c6412a31d2b44068c80743d0a8dba6883a
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-01-13 23:40:08 +0100

    Fix error when creating user

--- app/api/users.py
+++ app/api/users.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter
 
 from app import db
-from app.errors import InvalidRequest, NotFoundError
+from app.errors import InvalidRequest, NotFoundError, AlreadyTakenError
 from app.models import User
 from app.api.schemas.user import UserSchema, UserCreateSchema, UserUpdateSchema
 from . import permissions
@@ -28,9 +28,10 @@ async def get_user(user_id: int):
 
 
 @router.post('/', status_code=201, response_model=UserSchema)
-async def create_user(data: UserCreateSchema, logged_user=logged_user):
-    permissions.users.can_edit(logged_user)
-    print(data)
+async def create_user(data: UserCreateSchema):
+    user = db.session.query(User).filter_by(email=data.email).first()
+    if user:
+        raise AlreadyTakenError("email", data.email)
     try:
         user = User.from_data(data)
     except Exception as error:
@@ -44,7 +45,8 @@ async def create_user(data: UserCreateSchema, logged_user=logged_user):
 async def update_user(
     user_id: int, data: UserUpdateSchema, logged_user=logged_user
 ):
-    permissions.users.can_edit(logged_user)
+    if (logged_user.id != user_id):
+        permissions.users.can_edit(logged_user)
     user = await get_user(user_id)
     user.update(data)
     db.session.commit()
