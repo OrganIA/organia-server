4b0cd84a6b184e0b1d673ca35a7af72b9606f1ae
NicolasYapobi <you@example.com>
2022-01-14 15:30:11 +0900

    Add unit test

--- app/distance.py
+++ app/distance.py
@@ -1,15 +1,20 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
-geolocator = Nominatim(user_agent="my_request")
+geolocator = Nominatim(user_agent="Organia")
 
 
 def get_coordinates(address):
     location = geolocator.geocode(address)
-    position = [location.latitude, location.longitude]
+    position = (location.latitude, location.longitude)
     return position
 
 
-def get_distance(address_1, address_2):
-    distance = geodesic(get_coordinates(address_1), get_coordinates(address_2))
-    return distance
+def get_distance(pos_1, pos_2):
+    if pos_1 is None or pos_2 is None:
+        raise Exception("Fill both fields.")
+    try:
+        distance = geodesic(get_coordinates(pos_1), get_coordinates(pos_2))
+        return distance
+    except Exception:
+        raise Exception("One of theses addresses are not correct.")
---
+++ app/tests/test_distance.py
@@ -0,0 +1,11 @@
+from logging import raiseExceptions
+from unittest.case import _AssertRaisesContext
+from app import distance
+
+
+def test_get_coordinates():
+    assert distance.get_distance("PARIS") == (48.8588897, 2.3200410217200766)
+
+
+def test_get_distance():
+    assert distance.get_coordinates("PARIS", "MARSEILLE") == "661.6477355748142 km"
