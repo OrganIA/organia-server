9639279b26485de243dfdc25a921face65114f09
NicolasYapobi <you@example.com>
2022-01-17 21:18:26 +0900

    Fixed test

--- app/tests/test_distance.py
+++ app/tests/test_distance.py
@@ -4,8 +4,8 @@ from app import distance
 
 
 def test_get_coordinates():
-    assert distance.get_distance("PARIS") == (48.8588897, 2.3200410217200766)
+    assert distance.get_coordinates("PARIS") == (48.8588897, 2.3200410217200766)
 
 
 def test_get_distance():
-    assert distance.get_coordinates("PARIS", "MARSEILLE") == "661.6477355748142 km"
+    assert distance.get_distance("PARIS", "MARSEILLE") == "661.6477355748142 km"
