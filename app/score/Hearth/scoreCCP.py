c81b33957159d64ca0ce68cb79e8c917c6c16af1
GuillaumeBraux <guillaume.braux@epitech.eu>
2022-02-28 17:12:58 +0100

    add the folder Hearth and the file scoreCCP.py

---
+++ app/score/Hearth/scoreCCP.py
@@ -0,0 +1,13 @@
+def getDifAge(ageR, ageD):
+    ageRD = ageR - ageD
+    difAge = 0
+
+    if ageRD < 0:
+        difAge = (ageRD + 65) / 25
+    else:
+        difAge = 1 - (ageRD - 15) / 25
+    if ageR >= 18:
+        difAge = min(1, max(0, difAge))
+    else:
+        difAge = 1
+    return (difAge)
