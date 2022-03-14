bfe74db5891107b1fef995bc4d75ffab9477daf0
GuillaumeBraux <guillaume.braux@epitech.eu>
2022-03-14 21:20:22 +0100

    i added all the ICARi section

--- app/score/Hearth/hearth.py
+++ app/score/Hearth/hearth.py
@@ -4,55 +4,29 @@ from attr import NOTHING
 from matplotlib.pyplot import get
 from numpy import log as ln
 
-BILI2 = None
-DBILI2 = None
-DBNB2 = None
-CEC2 = None
-DRG2 = None
-CAT2 = None
-SIAV2 = None
-BNP2 = None
-PROBNP2 = None
-Date_Courante = None
-Delai_Var_Bio_LA = None
-DPROBNB2 = None
-F_Decile_PNj = None
-F_Ln_DFG_LAj = None
-F_Ln_BILI_LAj = None
-DIA2 = None
-CREAT2 = None
-DCREAT2 = None
-F_Decile_PNi = None
-F_Ln_DFG_LAi = None
-F_Ln_BILI_LAi = None
-ageR = None
-sexR = None
-urgence = None
-XPC = None
-
 #------------------------------------Index de risque Cardiaque du jour (ICARj)------------------------------------------
 
 #Fonction Décile des peptides natriurétiques (BNP ou NT-ProBNP) du jour
-def getF_Decile_PNj(CEC2, CAT2, SIAV2, DBNB2):
+def getF_Decile_PNj(CEC2, CAT2, SIAV2, DBNB2, BNP2, PROBNP2, Date_Courante, DPROBNB2, Delai_Var_Bio_LA, PROBNP, BNP):
    if CEC2 == 'O'or CAT2 == 'O' or SIAV2 == 'B':
        return 10
    elif BNP2 == NULL and PROBNP2 == NULL:
        return 1
    elif PROBNP2 != NULL and Date_Courante - DPROBNB2 <= Delai_Var_Bio_LA:
-        conditions = [928, 1478, 2044, 2661, 3416, 4406, 5645, 8000]
+        conditions = [928, 1478, 2044, 2661, 3416, 4406, 5645, 8000, 11332]
         res = [1,2,3,4,5,6,7,8,9]
-        if PROBNP2 >= 11332:
+        if PROBNP >= 11332:
             return 10
         for index, condition in enumerate(conditions):
-            if PROBNP2 < condition:
+            if PROBNP < condition:
                 return res[index]
    elif BNP2 != NULL and Date_Courante - DBNB2 <= Delai_Var_Bio_LA:
         conditions = [189, 314, 481, 622, 818, 1074, 1317, 1702, 2696]
         res = [1,2,3,4,5,6,7,8,9]
-        if BNP2 >= 2696:
+        if BNP >= 2696:
             return 10
         for index, condition in enumerate(conditions):
-            if BNP2 < condition:
+            if BNP < condition:
                 return res[index]
    else:
        return 1
@@ -64,7 +38,7 @@ def getF_DFGj(SEXR, AGER, CREAT2):
     else:
         return 186.3 * ((CREAT2/88.4)(-1.154)) * ((AGER)(-0.203)) * 1
 
-def getF_Ln_DFG_LAj(DIA2, CREAT2, DCREAT2, SEXR, AGER):
+def getF_Ln_DFG_LAj(DIA2, CREAT2, DCREAT2, SEXR, AGER, Date_Courante, Delai_Var_Bio_LA):
     F_DFGj = getF_DFGj(CREAT2, AGER, SEXR)
     if DIA2 == 'O':
         return ln(15)
@@ -74,7 +48,7 @@ def getF_Ln_DFG_LAj(DIA2, CREAT2, DCREAT2, SEXR, AGER):
         return ln(min(150, max(1, F_DFGj)))
 
 #Fonction Bilirubine en Liste d’attente du jour
-def getF_Ln_BILI_LAj(BILI2, DBILI2):
+def getF_Ln_BILI_LAj(BILI2, DBILI2, Date_Courante, Delai_Var_Bio_LA):
     if BILI2 == NULL or (Date_Courante - DBILI2) > Delai_Var_Bio_LA:
         return ln(5)
     else:
@@ -99,13 +73,72 @@ def getICARj(F_RisquePreGRFj, C_ICAR):
 
 #------------------------------------Index de risque avant perfusion ou implantation CEC (ICARi)------------------------
 
+#Fonction Assistance de Courte Durée
+def getF_ASCD(CEC2):
+        if CEC2 == 'O':
+            return 1
+        else:
+            return 0
+
+#Fonction Décile des peptides natriurétiques (BNP ou NT-ProBNP) initiale
+def getF_Decile_PNi(BNP_AVI, PBN_AVI,PROBNP, BNP, CEC2, CAT2, SIAV2):
+    if CEC2 == 'O' or CAT2 == 'O' or SIAV2 == 'B':
+        return 10
+    elif BNP_AVI == NULL and PBN_AVI == NULL:
+        return 1
+    elif PBN_AVI != NULL:
+        conditions = [928, 1478, 2044, 2661, 3416, 4406, 5645, 8000, 11332]
+        res = [1,2,3,4,5,6,7,8,9]
+        if PROBNP >= 11332:
+            return 10
+        for index, condition in enumerate(conditions):
+            if PROBNP < condition:
+                return res[index]
+    elif BNP_AVI != NULL:
+        conditions = [189, 314, 481, 622, 818, 1074, 1317, 1702, 2696]
+        res = [1,2,3,4,5,6,7,8,9]
+        if BNP >= 2696:
+            return 10
+        for index, condition in enumerate(conditions):
+            if BNP < condition:
+                return res[index]
+    else:
+        return 1
+
+#Fonction Débit de Filtration Glomérulaire en Liste d’attente (méthode MDRD) initiale
+def getF_DFGi(SEXR, CRE_AVI, AGER):
+    if SEXR == 'F':
+        return 186.3 * ((CRE_AVI/88.4)(-1.154)) * ((AGER)(-0.203)) * 0.742
+    else:
+        return 186.3 * ((CRE_AVI/88.4)(-1.154)) * ((AGER)(-0.203)) * 1
+
+def getF_Ln_DFG_LAi(DIA_AVI, CRE_AVI, SEXR, AGER):
+    F_DFGi = getF_DFGi(SEXR, CRE_AVI, AGER)
+    if DIA_AVI == 'O':
+        return ln(15)
+    elif CRE_AVI == NULL:
+        return ln(150)
+    else:
+        ln(min(150, max(1, F_DFGi)))
+
+#Fonction Bilirubine en Liste d’attente initiale
+def getF_Ln_BILI_LAi(BILI_AVI):
+    if BILI_AVI == NULL:
+        return ln(5)
+    else:
+        ln(min(230, max(5, BILI_AVI)))
+
+#La fonction de risque pré-greffe en liste d’attente initiale
 def getF_RisquePreGRFi(F_ASCD, F_Decile_PNi, F_Ln_DFG_LAi, F_Ln_BILI_LAi):
     return 1.301335 * F_ASCD + 0.157691 * F_Decile_PNi - 0.510058 * F_Ln_DFG_LAi + 0.615711 * F_Ln_BILI_LAi
 
+#Index de risque avant perfusion ou implantation CEC (ICARi)
 def getICARi(F_RisquePreGRFi, C_ICAR):
     return min(40, max(0, round((F_RisquePreGRFi - C_ICAR) * 10)))
 
 #-----------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------Calcul de l’Index de Risque Cardiaque (ICAR)---------------------------------------
 
 #La function de calcul de l’Index de Risque Cardiaque (ICAR)
 def getICAR(CEC2, DRG2, ICARj, ICARi):
@@ -114,17 +147,42 @@ def getICAR(CEC2, DRG2, ICARj, ICARi):
     else:
         return max(ICARj, ICARi)
 
-C_ICAR = 1.301335 * 0 + 0.157691 * 1 - 0.510058 * ln(150) + 0.615711 * ln(5)
+#-----------------------------------------------------------------------------------------------------------------------
 
-Decile_PNj = getF_Decile_PNj(CEC2, CAT2, SIAV2, DBNB2)
+
+DRG2 =  None
+CEC2 = None
+CAT2 = None
+SIAV2 = None
+DBNB2 = None
+DIA2 = None
+CREAT2 = None
+DCREAT2 = None
+sexR = None
+ageR = None
+BILI2 = None
+DBILI2 = None
+BILI_AVI = None
+DIA_AVI = None
+CRE_AVI = None
+BNP_AVI = None
+PBN_AVI = None
+PROBNP = None
+BNP = None
+
+C_ICAR = 1.301335 * 0 + 0.157691 * 1 - 0.510058 * ln(150) + 0.615711 * ln(5)
+F_Decile_PNj = getF_Decile_PNj(CEC2, CAT2, SIAV2, DBNB2)
 F_Ln_DFG_LAj = getF_Ln_DFG_LAj(DIA2, CREAT2, DCREAT2, sexR, ageR)
 F_Ln_BILI_LAj = getF_Ln_BILI_LAj(BILI2, DBILI2)
 F_ASCD = getF_ASCD(CEC2)
 F_RisquePreGRFj = getF_RisquePreGRFj(F_ASCD, F_Decile_PNj, F_Ln_DFG_LAj, F_Ln_BILI_LAj)
 ICARj = getICARj(F_RisquePreGRFj, C_ICAR)
 
+F_Ln_BILI_LAi = getF_Ln_BILI_LAi(BILI_AVI)
+F_Ln_DFG_LAi = getF_Ln_DFG_LAi(DIA_AVI, CRE_AVI, sexR, ageR)
+F_Decile_PNi = getF_Decile_PNi(BNP_AVI, PBN_AVI,PROBNP, BNP, CEC2, CAT2, SIAV2)
 F_RisquePreGRFi = getF_RisquePreGRFi(F_ASCD, F_Decile_PNi, F_Ln_DFG_LAi, F_Ln_BILI_LAi)
-ICARi = getICARi(F_RisquePreGRFi)
+ICARi = getICARi(F_RisquePreGRFi, C_ICAR)
 
 ICAR = getICAR(CEC2, DRG2, ICARj, ICARi)
 F_ICAR = 1000 * ICAR / 40
