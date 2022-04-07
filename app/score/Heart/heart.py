abe452a009d3da89eea5ef0f897e812f3157515a
GuillaumeBraux <guillaume.braux@epitech.eu>
2022-04-08 01:19:46 +0200

    add variable

--- app/score/Heart/heart.py
+++ app/score/Heart/heart.py
@@ -2,52 +2,53 @@ import numpy as np
 from scoreICAR import getICAR
 from scoreCCP import getScoreCCP
 from scoreCCB import getScoreCCB
+from datetime import datetime
 
 class Model:
-    def __init__(self):
-        self.ageR = 0
-        self.ageD = 0
-        self.sexD = 0
-        self.sexR = 0
-        self.tailleD = 0
-        self.tailleR = 0
-        self.poidsD = 0
-        self.poidsR = 0
+    def __init__(self, data):
+        self.ageR = data['R_D_NAI'] - datetime.now()
+        self.ageD = data['D_D_NAI'] - datetime.now()
+        self.sexD = data['sexD']
+        self.sexR = data['sexR']
+        self.tailleD = data['tailleD']
+        self.tailleR = data['tailleR']
+        self.poidsD = data['poidD']
+        self.poidsR = data['poidR']
 
-        self.DRG2 =  0
-        self.CEC2 = 0
+        self.DRG2 =  data['DRG']
+        self.CEC2 = data['CEC']
         self.CAT2 = 0
-        self.SIAV2 = 0
+        self.SIAV2 = data['SIAV']
         self.DBNB2 = 0
-        self.DIA2 = 0
-        self.CREAT2 = 0
-        self.DCREAT2 = 0
+        self.DIA2 = data['DIA']
+        self.CREAT2 = data['CREAT']
+        self.DCREAT2 = data['DCREAT']
         self.BILI2 = 0
-        self.DBILI2 = 0
-        self.BILI_AVI = 0
-        self.DIA_AVI = 0
-        self.CRE_AVI = 0
-        self.BNP_AVI = 0
-        self.PBN_AVI = 0
+        self.DBILI2 = data['DBILI']
+        self.BILI_AVI = data['BILI_AVI']
+        self.DIA_AVI = data['CAT']
+        self.CRE_AVI = data['CRE_AVI']
+        self.BNP_AVI = data['BNP_AVI']
+        self.PBN_AVI = data['PBN_AVI']
         self.PROBNP2 = 0
-        self.PROBNP = 0
+        self.PROBNP = data['PROBNP']
         self.Date_Courante = 0
-        self.DPROBNB2 = 0
+        self.DPROBNB2 = data['DPROBNP']
         self.Delai_Var_Bio_LA = 0
-        self.BNP = 0
+        self.BNP = data['BNP']
         self.BNP2 = 0
 
-        self.urgence = 0
+        self.urgence = data['URGENCE']
         self.XPC = 0
         self.KXPC = 0
         self.DAURG = 0
         self.DA = 0
 
-        self.MAL = 0
-        self.MAL2 = 0
-        self.MAL3 = 0
-        self.BILI = 0
-        self.dateDBILI = 0
+        self.MAL = data['MALADIE1']
+        self.MAL2 = data['MALADIE2']
+        self.MAL3 = data['MALADIE3']
+        self.BILI = data['BILI']
+        self.dateDBILI = data['DBILI']
         self.dVarBio = 0
         self.DFG = 0
         self.ABOD = 0
