[FIX] all overflow and date errors

app/score/Heart/scoreCCP.py
@@ -2,6 +2,13 @@ import numpy as np
 
 # Fonction d’appariement en âge entre donneur et receveur
 def getDifAge(ageR, ageD):
+    x= np.timedelta64(ageR, 'ns')
+    day = x.astype('timedelta64[D]')
+    ageR = day.astype(int)
+    xd= np.timedelta64(ageD, 'ns')
+    dayd = xd.astype('timedelta64[D]')
+    ageD = dayd.astype(int)
+
     ageRD = ageR - ageD
     difAge = 0
 
@@ -26,6 +33,9 @@ def getABO(ABOD, ABOR):
 
 # Appariement morphologique entre donneur et receveur
 def getSC(tailleD, tailleR, poidsD, poidsR, ageR, sexD):
+    x= np.timedelta64(ageR, 'ns')
+    day = x.astype('timedelta64[D]')
+    ageR = day.astype(int)
 
     fscD = 0.007184 * pow(tailleD,0.725) * pow(poidsD,0.725)
     fscR = 0.007184 * pow(tailleR,0.725) * pow(poidsR,0.725)
@@ -50,6 +60,10 @@ def getRiskPostGRF(fageR, fageD, fMAL, LnBili, LnDFG, sexRD):
 
 # Fonction sur l’âge du receveur
 def getFager(ageR):
+    x= np.timedelta64(ageR, 'ns')
+    day = x.astype('timedelta64[D]')
+    ageR = day.astype(int)
+
     if ageR > 50:
         return 1
     else:
@@ -64,6 +78,11 @@ def getfMAL(MAL, MAL2, MAL3):
 
 # Fonction bilirubine pour le post-greffe
 def getLnBili(BILI, dateDBILI, dVarBio):
+    dateDBILI = dateDBILI.timestamp()
+    xd= np.timedelta64(dVarBio, 'ns')
+    dayd = xd.astype('timedelta64[D]')
+    dVarBio = dayd.astype(int)
+
     if np.isnan(BILI) == True or dateDBILI > dVarBio:
         return np.log(230)
     else:
@@ -71,6 +90,7 @@ def getLnBili(BILI, dateDBILI, dVarBio):
 
 # Fonction du Débit de Filtration Glomérulaire pour le post-greffe
 def getLnDFG(DIA, CREAT, DCREAT, dVarBio, DFG):
+    DCREAT = DCREAT.timestamp()
     if DIA == 'O':
         return np.log(15)
     elif np.isnan(CREAT) == True or DCREAT > dVarBio:
@@ -86,6 +106,10 @@ def getsexRD(sexD, sexR):
         return 0
 # Fonction sur l’âge du donneur
 def getfageD(ageD):
+    x= np.timedelta64(ageD, 'ns')
+    day = x.astype('timedelta64[D]')
+    ageD = day.astype(int)
+
     if ageD > 55:
         return 1
     else:
@@ -93,10 +117,10 @@ def getfageD(ageD):
 # ********************Score CCP******************
 
 def getScoreCCP(model, CCB):
-    LnDFG = getLnDFG(model.DIA, model.CREAT, model.DCREAT, model.Delai_Var_Bio_GRF, model.DFG)
+    LnDFG = getLnDFG(model.DIA, model.CREAT, model.DCREAT, model.DelaiVarBioGRF, model.F_DFG)
     fageD = getfageD(model.ageD)
     sexRD = getsexRD(model.sexD, model.sexR)
-    LnBili = getLnBili(model.BILI, model.dateDBILI, model.Delai_Var_Bio_GRF)
+    LnBili = getLnBili(model.BILI, model.dateDBILI, model.DelaiVarBioGRF)
     fMAL = getfMAL(model.MAL, model.MAL2, model.MAL3)
     fageR = getFager(model.ageR)
     riskPostGRF = getRiskPostGRF(fageR, fageD, fMAL, LnBili, LnDFG, sexRD)

app/score/Heart/scoreICAR.py
@@ -1,5 +1,6 @@
 from cmath import isnan
 from pandas import to_datetime
+import pandas as pd
 from math import ceil as round
 from tkinter.tix import Tree
 from matplotlib.pyplot import get
@@ -35,6 +36,9 @@ def getF_Decile_PNj(CEC, CAT, SIAV, DBNP, BNP, PROBNP, Date_Courante, DPROBNB, D
 
 #Fonction Débit de Filtration Glomérulaire en Liste d’attente (méthode MDRD) du jour
 def getF_DFGj(SEXR, AGER, CREAT):
+    x= np.timedelta64(AGER, 'ns')
+    day = x.astype('timedelta64[D]')
+    AGER = day.astype(int)
     if SEXR == 'F':
         return 186.3 * (pow((CREAT/88.4),-1.154)) * (AGER*-0.203) * 0.742
     else:
@@ -42,16 +46,22 @@ def getF_DFGj(SEXR, AGER, CREAT):
 
 def getF_Ln_DFG_LAj(DIA, CREAT, DCREAT, SEXR, AGER, Date_Courante, Delai_Var_Bio_LA):
     F_DFGj = getF_DFGj(SEXR, AGER, CREAT)
+    x= np.timedelta64((Date_Courante - DCREAT), 'ns')
+    day = x.astype('timedelta64[D]')
+    DCDCRSub = day.astype(int)
     if DIA == 'O':
         return ln(15)
-    elif isnan(CREAT) == True or (Date_Courante - DCREAT) > Delai_Var_Bio_LA:
+    elif isnan(CREAT) == True or DCDCRSub > Delai_Var_Bio_LA:
         return ln(150)
     else:
         return ln(min(150, max(1, F_DFGj)))
 
 #Fonction Bilirubine en Liste d’attente du jour
 def getF_Ln_BILI_LAj(BILI, DBILI, Date_Courante, Delai_Var_Bio_LA):
-    if isnan(BILI) == True or (Date_Courante - DBILI) > Delai_Var_Bio_LA:
+    x= np.timedelta64((Date_Courante - DBILI), 'ns')
+    day = x.astype('timedelta64[D]')
+    DCDBLSub = day.astype(int)
+    if isnan(BILI) == True or DCDBLSub > Delai_Var_Bio_LA:
         return ln(5)
     else:
         return ln(min(230, max(5, BILI)))
@@ -158,7 +168,7 @@ def getICAR(model):
     F_RisquePreGRFi = getF_RisquePreGRFi(F_ASCD, F_Decile_PNi, F_Ln_DFG_LAi, F_Ln_BILI_LAi)
     ICARi = getICARi(F_RisquePreGRFi, C_ICAR)
 
-    if model.CEC != 'O' and model.DRG2 != 'O':
+    if model.CEC != 'O' and model.DRG != 'O':
         return ICARj
     else:
         return max(ICARj, ICARi)