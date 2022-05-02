81d1d67e31821398ec7914ea5085b2f78002ea9d
GuillaumeBraux <guillaume.braux@epitech.eu>
2022-05-02 08:16:08 +0200

    Update of the ScoreICAR to v3 and now the dates are decimals

--- app/score/Heart/heart.py
+++ app/score/Heart/heart.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scoreICAR import getICAR
+from scoreICAR import getICAR, checkICAR
 from scoreCCP import getScoreCCP
 from scoreCCB import getScoreCCB
 from datetime import datetime
@@ -8,8 +8,8 @@ from test import sample1
 
 class Model:
     def __init__(self, data):
-        self.ageR = data['D_INSC'].year - data['R_D_NAI'].year
-        self.ageD = data['D_INSC'].year - data['R_D_NAI'].year
+        self.ageR = (data['D_INSC'] - data['R_D_NAI']).days / 365
+        self.ageD = (data['D_INSC'] - data['R_D_NAI']).days / 365
         self.sexD = data['SEXD']
         self.sexR = data['SEXR']
         self.tailleD = data['TAILLED']
@@ -19,6 +19,7 @@ class Model:
 
         self.DRG = data['DRG']
         self.CEC = data['CEC']
+        self.DCEC = data['DCEC']
         self.CAT = data['CAT']
         self.SIAV = data['SIAV']
         self.DIA = data['DIA']
@@ -39,8 +40,8 @@ class Model:
         self.urgence = data['URGENCE']
         self.XPC = data['XPC']
         self.KXPC = data['KXPC']
-        self.DAURG = (data['D_PREL'].year - data['D_URGENCE'].year) * 12 + (data['D_PREL'].month - data['D_URGENCE'].month)
-        self.DA = (data['D_PREL'].year - data['D_INSC'].year) * 12 + (data['D_PREL'].month - data['D_INSC'].month)
+        self.DAURG = (data['D_PREL'] - data['D_URGENCE']).days / 12
+        self.DA = (data['D_PREL'] - data['D_INSC']).days / 12
 
         self.MAL = data['MAL']
         self.MAL2 = data['MAL2']
@@ -69,8 +70,9 @@ def getScoreNACG(scoreCCP, TTLGP):
 # ***********************************************************
 model = Model(sample1)
 
-scoreICAR = getICAR(model)
+scoreICAR = checkICAR(getICAR(model))
 F_ICAR = 1000 * scoreICAR / 40
 scoreCCB = getScoreCCB(model, F_ICAR)
 scoreCCP = getScoreCCP(model, scoreCCB)
 NAGC = getScoreNACG(scoreCCP, model.TTLGP)
+print(NAGC)
--- app/score/Heart/scoreICAR.py
+++ app/score/Heart/scoreICAR.py
@@ -1,100 +1,103 @@
 from cmath import isnan
 from math import ceil as round
 from numpy import log as ln
-import numpy as np
 
-#------------------------------------Index de risque Cardiaque du jour (ICARj)------------------------------------------
 
-#Fonction Décile des peptides natriurétiques (BNP ou NT-ProBNP) du jour
+# ------------------------------------Index de risque Cardiaque du jour (ICARj)------------------------------------------
+
+# Fonction Décile des peptides natriurétiques (BNP ou NT-ProBNP) du jour
 def getF_Decile_PNj(CEC, CAT, SIAV, DBNP, BNP, PROBNP, Date_Courante, DPROBNB, Delai_Var_Bio_LA):
-    if CEC == 'O'or CAT == 'O' or SIAV == 'B':
-       return 10
-    elif isnan(BNP) == True and isnan(PROBNP) == True:
-       return 1
-    elif isnan(PROBNP) != True and (Date_Courante - DPROBNB).days <= Delai_Var_Bio_LA:
+    if CEC == 'O' or CAT == 'O' or SIAV == 'B':
+        return 10
+    elif isnan(BNP) is True and isnan(PROBNP) is True:
+        return 1
+    elif isnan(PROBNP) is not True and (Date_Courante - DPROBNB).days <= Delai_Var_Bio_LA:
         conditions = [928, 1478, 2044, 2661, 3416, 4406, 5645, 8000, 11332]
-        res = [1,2,3,4,5,6,7,8,9]
+        res = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if PROBNP >= 11332:
             return 10
         for index, condition in enumerate(conditions):
             if PROBNP < condition:
                 return res[index]
-    elif isnan(BNP) != True and Date_Courante - DBNP <= Delai_Var_Bio_LA:
+    elif isnan(BNP) is not True and (Date_Courante - DBNP).days <= Delai_Var_Bio_LA:
         conditions = [189, 314, 481, 622, 818, 1074, 1317, 1702, 2696]
-        res = [1,2,3,4,5,6,7,8,9]
+        res = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if BNP >= 2696:
             return 10
         for index, condition in enumerate(conditions):
             if BNP < condition:
                 return res[index]
     else:
-       return 1
+        return 1
 
-#Fonction Débit de Filtration Glomérulaire en Liste d’attente (méthode MDRD) du jour
+
+# Fonction Débit de Filtration Glomérulaire en Liste d’attente (méthode MDRD) du jour
 def getF_DFGj(SEXR, AGER, CREAT):
+    #print(SEXR, AGER, CREAT)
     if SEXR == 'F':
-        return 186.3 * (pow((CREAT/88.4),-1.154)) * (AGER*-0.203) * 0.742
+        return 186.3 * (pow((CREAT / 88.4), -1.154)) * (AGER * -0.203) * 0.742
     else:
-        return 186.3 * (pow((CREAT/88.4),-1.154)) * (AGER*-0.203) * 1
+        return 186.3 * (pow((CREAT / 88.4), -1.154)) * (AGER * -0.203) * 1
+
 
 def getF_Ln_DFG_LAj(DIA, CREAT, DCREAT, SEXR, AGER, Date_Courante, Delai_Var_Bio_LA):
     F_DFGj = getF_DFGj(SEXR, AGER, CREAT)
-    x= np.timedelta64((Date_Courante - DCREAT), 'ns')
-    day = x.astype('timedelta64[D]')
-    DCDCRSub = day.astype(int)
+    #print(F_DFGj)
     if DIA == 'O':
         return ln(15)
-    elif isnan(CREAT) == True or DCDCRSub > Delai_Var_Bio_LA:
+    elif isnan(CREAT) is True or (Date_Courante - DCREAT).days > Delai_Var_Bio_LA:
         return ln(150)
     else:
         return ln(min(150, max(1, F_DFGj)))
 
-#Fonction Bilirubine en Liste d’attente du jour
+
+# Fonction Bilirubine en Liste d’attente du jour
 def getF_Ln_BILI_LAj(BILI, DBILI, Date_Courante, Delai_Var_Bio_LA):
-    x= np.timedelta64((Date_Courante - DBILI), 'ns')
-    day = x.astype('timedelta64[D]')
-    DCDBLSub = day.astype(int)
-    if isnan(BILI) == True or DCDBLSub > Delai_Var_Bio_LA:
+    if isnan(BILI) is True or (Date_Courante - DBILI).days > Delai_Var_Bio_LA:
         return ln(5)
     else:
         return ln(min(230, max(5, BILI)))
+
+
+# Fonction Assistance de Courte Durée
+def getF_ASCD(CEC):
+    if CEC == 'O':
+        return 1
+    else:
+        return 0
+
 
 # La fonction de risque pré-greffe en liste d’attente du jour
 def getF_RisquePreGRFj(F_ASCD, F_Decile_PNj, F_Ln_DFG_LAj, F_Ln_BILI_LAj):
     return 1.301335 * F_ASCD + 0.157691 * F_Decile_PNj - 0.510058 * F_Ln_DFG_LAj + 0.615711 * F_Ln_BILI_LAj
 
-#La function Index de risque Cardiaque du jour (ICARj)
+
+# La function Index de risque Cardiaque du jour (ICARj)
 def getICARj(F_RisquePreGRFj, C_ICAR):
-    return max(0, round((F_RisquePreGRFj - C_ICAR) * 10))
+    return min(40, max(0, round((F_RisquePreGRFj - C_ICAR) * 10)))
 
-#-----------------------------------------------------------------------------------------------------------------------
 
-#------------------------------------Index de risque avant perfusion ou implantation CEC (ICARi)------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
-#Fonction Assistance de Courte Durée
-def getF_ASCD(CEC):
-        if CEC == 'O':
-            return 1
-        else:
-            return 0
+# ------------------------------------Index de risque avant perfusion ou implantation CEC (ICARi)------------------------
 
-#Fonction Décile des peptides natriurétiques (BNP ou NT-ProBNP) initiale
-def getF_Decile_PNi(BNP_AVI, PBN_AVI,PROBNP, BNP, CEC, CAT, SIAV):
+# Fonction Décile des peptides natriurétiques (BNP ou NT-ProBNP) initiale
+def getF_Decile_PNi(BNP_AVI, PBN_AVI, PROBNP, BNP, CEC, CAT, SIAV):
     if CEC == 'O' or CAT == 'O' or SIAV == 'B':
         return 10
-    elif isnan(BNP_AVI) == True and isnan(PBN_AVI) == True:
+    elif isnan(BNP_AVI) is True and isnan(PBN_AVI) is True:
         return 1
-    elif isnan(PBN_AVI) != True:
+    elif isnan(PBN_AVI) is not True:
         conditions = [928, 1478, 2044, 2661, 3416, 4406, 5645, 8000, 11332]
-        res = [1,2,3,4,5,6,7,8,9]
+        res = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if PROBNP >= 11332:
             return 10
         for index, condition in enumerate(conditions):
             if PROBNP < condition:
                 return res[index]
-    elif isnan(BNP_AVI) != True:
+    elif isnan(BNP_AVI) is not True:
         conditions = [189, 314, 481, 622, 818, 1074, 1317, 1702, 2696]
-        res = [1,2,3,4,5,6,7,8,9]
+        res = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if BNP >= 2696:
             return 10
         for index, condition in enumerate(conditions):
@@ -103,45 +106,53 @@ def getF_Decile_PNi(BNP_AVI, PBN_AVI,PROBNP, BNP, CEC, CAT, SIAV):
     else:
         return 1
 
-#Fonction Débit de Filtration Glomérulaire en Liste d’attente (méthode MDRD) initiale
+
+# Fonction Débit de Filtration Glomérulaire en Liste d’attente (méthode MDRD) initiale
 def getF_DFGi(SEXR, CRE_AVI, AGER):
     if SEXR == 'F':
-        return 186.3 * ((CRE_AVI/88.4)*-1.154) * (AGER*(-0.203)) * 0.742
+        return 186.3 * ((CRE_AVI / 88.4) * -1.154) * (AGER * (-0.203)) * 0.742
     else:
-        return 186.3 * ((CRE_AVI/88.4)*-1.154) * (AGER*(-0.203)) * 1
+        return 186.3 * ((CRE_AVI / 88.4) * -1.154) * (AGER * (-0.203)) * 1
+
 
 def getF_Ln_DFG_LAi(DIA_AVI, CRE_AVI, SEXR, AGER):
     F_DFGi = getF_DFGi(SEXR, CRE_AVI, AGER)
     if DIA_AVI == 'O':
         return ln(15)
-    elif isnan(CRE_AVI) == True:
+    elif isnan(CRE_AVI) is True:
         return ln(150)
     else:
         return ln(min(150, max(1, F_DFGi)))
 
-#Fonction Bilirubine en Liste d’attente initiale
+
+# Fonction Bilirubine en Liste d’attente initiale
 def getF_Ln_BILI_LAi(BILI_AVI):
-    if isnan(BILI_AVI) == True:
+    if isnan(BILI_AVI) is True:
         return ln(5)
     else:
         return ln(min(230, max(5, BILI_AVI)))
 
-#La fonction de risque pré-greffe en liste d’attente initiale
+
+# La fonction de risque pré-greffe en liste d’attente initiale
 def getF_RisquePreGRFi(F_ASCD, F_Decile_PNi, F_Ln_DFG_LAi, F_Ln_BILI_LAi):
     return 1.301335 * F_ASCD + 0.157691 * F_Decile_PNi - 0.510058 * F_Ln_DFG_LAi + 0.615711 * F_Ln_BILI_LAi
 
-#Index de risque avant perfusion ou implantation CEC (ICARi)
+
+# Index de risque avant perfusion ou implantation CEC (ICARi)
 def getICARi(F_RisquePreGRFi, C_ICAR):
     return min(40, max(0, round((F_RisquePreGRFi - C_ICAR) * 10)))
 
-#-----------------------------------------------------------------------------------------------------------------------
 
-#------------------------------------Calcul de l’Index de Risque Cardiaque (ICAR)---------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
+
+# ------------------------------------Calcul de l’Index de Risque Cardiaque (ICAR)---------------------------------------
 
 def getICAR(model):
     C_ICAR = 1.301335 * 0 + 0.157691 * 1 - 0.510058 * ln(150) + 0.615711 * ln(5)
-    F_Decile_PNj = getF_Decile_PNj(model.CEC, model.CAT, model.SIAV, model.DBNP, model.BNP, model.PROBNP, model.Date_Courante, model.DPROBNB, model.DelaiVarBioGRF)
-    F_Ln_DFG_LAj = getF_Ln_DFG_LAj(model.DIA, model.CREAT, model.DCREAT, model.sexR, model.ageR, model.Date_Courante, model.DelaiVarBioGRF)
+    F_Decile_PNj = getF_Decile_PNj(model.CEC, model.CAT, model.SIAV, model.DBNP, model.BNP, model.PROBNP,
+                                   model.Date_Courante, model.DPROBNB, model.DelaiVarBioGRF)
+    F_Ln_DFG_LAj = getF_Ln_DFG_LAj(model.DIA, model.CREAT, model.DCREAT, model.sexR, model.ageR, model.Date_Courante,
+                                   model.DelaiVarBioGRF)
     F_Ln_BILI_LAj = getF_Ln_BILI_LAj(model.BILI, model.DBILI, model.Date_Courante, model.DelaiVarBioGRF)
     F_ASCD = getF_ASCD(model.CEC)
     F_RisquePreGRFj = getF_RisquePreGRFj(F_ASCD, F_Decile_PNj, F_Ln_DFG_LAj, F_Ln_BILI_LAj)
@@ -149,18 +160,26 @@ def getICAR(model):
 
     F_Ln_BILI_LAi = getF_Ln_BILI_LAi(model.BILI_AVI)
     F_Ln_DFG_LAi = getF_Ln_DFG_LAi(model.DIA_AVI, model.CRE_AVI, model.sexR, model.ageR)
-    F_Decile_PNi = getF_Decile_PNi(model.BNP_AVI, model.PBN_AVI, model.PROBNP, model.BNP, model.CEC, model.CAT, model.SIAV)
+    F_Decile_PNi = getF_Decile_PNi(model.BNP_AVI, model.PBN_AVI, model.PROBNP, model.BNP, model.CEC, model.CAT,
+                                   model.SIAV)
     F_RisquePreGRFi = getF_RisquePreGRFi(F_ASCD, F_Decile_PNi, F_Ln_DFG_LAi, F_Ln_BILI_LAi)
     ICARi = getICARi(F_RisquePreGRFi, C_ICAR)
+
+    #print('C_ICAR', C_ICAR)
+    #print('F_Decile_PNj', F_Decile_PNj)
+    #print( 'F_Ln_DFG_LAj',  F_Ln_DFG_LAj)
+    #print('F_Ln_BILI_LAj', F_Ln_BILI_LAj)
+    #print('F_ASCD', F_ASCD)
 
     if model.CEC != 'O' and model.DRG != 'O':
         return ICARj
     else:
         return max(ICARj, ICARi)
 
+
 def checkICAR(ICAR):
     if ICAR > 40 or ICAR < 0:
-        raise Exception ("Le score ICAR doit etre compris entre 0 et 40")
+        raise Exception("Le score ICAR doit etre compris entre 0 et 40")
     else:
-        return 0
-#-----------------------------------------------------------------------------------------------------------------------
+        return ICAR
+# -----------------------------------------------------------------------------------------------------------------------
