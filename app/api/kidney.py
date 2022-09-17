7e3bdb30b97b1f26d4d9fc71181226455cccb536
Félix <felix@burdot.com>
2022-09-17 23:26:08 +0200

    Add route to update kidney score

--- app/api/kidney.py
+++ app/api/kidney.py
@@ -10,6 +10,13 @@ from app.api.schemas.kidney import (
 
 router = APIRouter(prefix='/kidneys')
 
+
+@router.get('/{listing_id}', response_model=KidneySchema)
+async def get_kidneys(listing_id: int):
+    query = db.session.query(Kidney).filter_by(listing_id=listing_id).first()
+    return query
+
+
 @router.post('/{listing_id}', status_code=201, response_model=KidneySchema)
 async def create_kidney_variables(listing_id: int, data: KidneySchema):
     listing = db.session.query(Listing).filter_by(id=listing_id).first()
@@ -23,4 +30,15 @@ async def create_kidney_variables(listing_id: int, data: KidneySchema):
     data.listing_id = listing_id
     data = data.dict(exclude_unset=True)
     kidney = db.add(Kidney, data)
+    return kidney
+
+
+@router.post('/{listing_id}/score', status_code=201, response_model=KidneySchema)
+async def update_kidney_score(listing_id: int, score: KidneyUpdateScore):
+    listing = db.session.query(Listing).filter_by(id=listing_id).first()
+    if listing == None:
+        raise NotFoundError('Listing not found')
+    kidney = await get_kidneys(listing_id)
+    kidney.score = score.score
+    db.session.commit()
     return kidney
