875d9f132c3955dfe9d1c6198a2eea9a2efe2675
OkilSaber <okil.lakhdari@kbrwadventure.com>
2022-09-18 21:16:06 +0200

    Add organs fields

--- app/api/organs.py
+++ app/api/organs.py
@@ -1,16 +1,19 @@
 from fastapi import APIRouter
 
 from app.errors import NotFoundError
-from app.models import Lung
+from app.models import Lung, HeartScore, Kidney
 
 router = APIRouter(prefix='/organs')
 
 
 @router.get('/{organ_name}')
 async def get_organ_required_fields(organ_name: str):
-
-    print(organ_name)
-    if organ_name == 'lungs':
-        return Lung.__table__.columns.keys()
-    else:
-        raise NotFoundError('No such organ found')
+    match organ_name:
+        case 'lung':
+            return Lung.__table__.columns.keys()
+        case 'heart':
+            return HeartScore.__table__.columns.keys()
+        case 'kidney':
+            return Kidney.__table__.columns.keys()
+        case _:
+            raise NotFoundError
