b16195328b55195af2d452d074b98e80b600503b
Félix <felix@burdot.com>
2022-10-09 18:16:07 +0200

    Add endpoint that returns listings with computed lung score

--- app/api/lungs.py
+++ app/api/lungs.py
@@ -5,8 +5,10 @@ from app import db
 from app.errors import NotFoundError, InvalidRequest
 from app.models import Lung, Listing
 from app.api.schemas.lung import (
-    LungSchema, LungCreateSchema, LungUpdateScore
+    LungSchema, LungCreateSchema
 )
+from app.api.persons import get_person
+from app.score.Lungs.LungsScore import lungs_final_score
 
 router = APIRouter(prefix='/lungs')
 
@@ -36,12 +38,12 @@ async def update_lungs_variables(listing_id: int, data: LungCreateSchema):
 
 
 @router.post('/{listing_id}/score', status_code=201, response_model=LungSchema)
-async def update_lungs_score(listing_id: int, score: LungUpdateScore):
+async def update_lungs_score(listing_id: int, score: float):
     listing = db.session.query(Listing).filter_by(id=listing_id).first()
     if listing == None:
         raise NotFoundError('Listing not found')
     lung = await get_lungs(listing_id)
-    lung.score = score.score
+    lung.score = score
     db.session.commit()
     return lung
 
@@ -56,3 +58,15 @@ async def delete_lungs_score(listing_id: int):
     lung.score = 0.0
     db.session.commit()
     return
+
+
+@router.get('/{listing_id}/matches', status_code=201)
+async def compute_matches(listing_id: int):
+    listing_lungs_receivers = db.session.query(Listing).filter(
+        Listing.id != listing_id, Listing.donor == False, Listing.organ == "LUNG").all()
+    listing_lungs_receiver_ids = []
+    for listing_lungs_receiver in listing_lungs_receivers:
+        listing_lungs_receiver_ids.append(listing_lungs_receiver.id)
+        person_receiver = await get_person(listing_lungs_receiver.person_id)
+        await update_lungs_score(listing_lungs_receiver.id, lungs_final_score(person_receiver, None, listing_lungs_receiver))
+    return db.session.query(Lung).filter(Lung.listing_id.in_(listing_lungs_receiver_ids)).all()
