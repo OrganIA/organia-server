fadc478664375f40fb487d90038879257e19e4ac
NicolasYapobi <nicolas.yapobi@epitech.eu>
2022-11-28 01:13:45 +0100

    Add persons routes Add enums helper

---
+++ app/api/person.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+from app import db
+from app.db.models import Person
+from app.errors import NotFoundError
+from app.utils.bp import Blueprint
+from app.utils.static import Static
+
+bp = Blueprint('persons', auth=True)
+
+
+class PersonSchema(Static):
+    @staticmethod
+    def birth_date(d):
+        birth_date = Static._get(d, 'birth_date')
+        return datetime.fromisoformat(birth_date).date()
+
+    first_name = str
+    last_name = str
+    description = str
+    abo = Person.ABO
+    rhesus = Person.Rhesus
+    gender = Person.Gender
+
+
+def update(person, data):
+    if data["rhesus"] == '+':
+        person.rhesus = Person.Rhesus.POSITIVE
+    else:
+        person.rhesus = Person.Rhesus.NEGATIVE
+    person.first_name = data["first_name"]
+    person.last_name = data["last_name"]
+    person.description = data["description"]
+    person.abo = data["abo"]
+    person.gender = data["gender"]
+    return person
+
+
+@bp.get('/')
+def get_persons():
+    return db.session.query(Person)
+
+
+@bp.get('/<int:id>')
+def get_person(id):
+    result = db.session.get(Person, id)
+    if not result:
+        raise NotFoundError
+    return result
+
+
+@bp.post('/')
+def create_person(data: PersonSchema):
+    person = Person(**data.dict)
+    db.session.add(person)
+    db.session.commit()
+    return get_person(person.id)
+
+
+@bp.post('/<int:id>')
+def update_person(id, data):
+    person = update(get_person(id), data)
+    db.session.commit()
+    return person
+
+
+@bp.delete('/<int:id>')
+def delete_person(id: int):
+    person = get_person(id)
+    db.session.delete(person)
+    db.session.commit()
