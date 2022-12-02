322427bfc34901f8a48c6ce97c895f69f6828b17
NicolasYapobi <nicolas.yapobi@epitech.eu>
2022-12-02 17:54:11 +0100

    Update init model

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,13 +1,14 @@
 # isort: off
 # Generated, do not modify manually
 
-from .city import City as City
-from .user import User as User
-from .staff import Staff as Staff
-from .person import Person as Person
-from .message import Message as Message
 from .listing import Listing as Listing
-from .chat import Chat as Chat
 from .login_token import LoginToken as LoginToken
+from .city import City as City
+from .staff import Staff as Staff
 from .hospital import Hospital as Hospital
+from .message import Message as Message
 from .chat_group import ChatGroup as ChatGroup
+from .liver import Liver as Liver
+from .person import Person as Person
+from .user import User as User
+from .chat import Chat as Chat
